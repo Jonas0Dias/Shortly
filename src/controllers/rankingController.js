c2139f37a51a2e3f7269bc6b4d57777f4370a3f7
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-03-03 13:29:04 -0300

    Route GET/ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -4,14 +4,13 @@ import { db } from "../config/database.js";
 export async function visitCount(req,res){
 
    try{
-
+    const ranking = await db.query(`SELECT "users"."id" as "user_id","users"."name" as "username", COUNT(*) as "linksCount" ,SUM("urls"."visitcount") as "visitCount" from tokens  JOIN users ON "tokens"."user_id"="users"."id" JOIN "urls"ON "urls"."token" = "tokens".token GROUP BY "users"."id" ORDER BY "linksCount" ASC LIMIT 10`)						
+    res.status(200).send(ranking.rows)
    }
     catch(err){
-        
+        res.status(500).send(err.message)
     }
 } 
 
-"users".id,"users".name 
 
 
-SELECT SUM(visitcount) as visitcountSum "tokens"."user_id", "urls"."url","urls"."shortUrl","urls"."visicount" FROM "tokens" JOIN "urls" ON "urls".token = "tokens".token
