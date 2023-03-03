9bf5de3681d9312cddfdbcf3582848f880a6a435
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-03-03 16:40:08 -0300

    try fix bug avaliador

--- dump.sql
+++ dump.sql
@@ -1,295 +1,76 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 15.2
--- Dumped by pg_dump version 15.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: public; Type: SCHEMA; Schema: -; Owner: jonas
---
-
--- *not* creating schema, since initdb creates it
+CREATE DATABASE shortly
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
 
 
-ALTER SCHEMA public OWNER TO jonas;
+-- Table: public.tokens
 
-SET default_tablespace = '';
+-- DROP TABLE IF EXISTS public.tokens;
 
-SET default_table_access_method = heap;
-
---
--- Name: tokens; Type: TABLE; Schema: public; Owner: jonas
---
-
-CREATE TABLE public.tokens (
-    id integer NOT NULL,
+CREATE TABLE IF NOT EXISTS public.tokens
+(
+    id integer NOT NULL DEFAULT 'nextval('tokens_id_seq'::regclass)',
     user_id integer NOT NULL,
-    token character(36) NOT NULL
-);
+    token character varying(36) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT tokens_pkey PRIMARY KEY (id),
+    CONSTRAINT tokens_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tokens
+    OWNER to postgres;
+
+    -- Table: public.urls
+
+-- DROP TABLE IF EXISTS public.urls;
+
+CREATE TABLE IF NOT EXISTS public.urls
+(
+    id integer NOT NULL DEFAULT 'nextval('urls_id_seq'::regclass)',
+    token character varying(36) COLLATE pg_catalog."default" NOT NULL,
+    url character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    "shortUrl" character varying(8) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT urls_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.urls
+    OWNER to postgres;
+
+    -- Table: public.users
+
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL DEFAULT 'nextval('users_id_seq'::regclass)',
+    email character varying(100) COLLATE pg_catalog."default",
+    password text COLLATE pg_catalog."default" NOT NULL,
+    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    "createDate" timestamp without time zone,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
 
-
-ALTER TABLE public.tokens OWNER TO jonas;
-
---
--- Name: tokens_id_seq; Type: SEQUENCE; Schema: public; Owner: jonas
---
-
-CREATE SEQUENCE public.tokens_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.tokens_id_seq OWNER TO jonas;
-
---
--- Name: tokens_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jonas
---
-
-ALTER SEQUENCE public.tokens_id_seq OWNED BY public.tokens.id;
-
-
---
--- Name: urls; Type: TABLE; Schema: public; Owner: jonas
---
-
-CREATE TABLE public.urls (
-    id integer NOT NULL,
-    token character varying(36) NOT NULL,
-    url character varying(255) NOT NULL,
-    "shortUrl" character varying(8) NOT NULL
-);
-
-
-ALTER TABLE public.urls OWNER TO jonas;
-
---
--- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: jonas
---
-
-CREATE SEQUENCE public.urls_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.urls_id_seq OWNER TO jonas;
-
---
--- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jonas
---
-
-ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: jonas
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    email character varying(100),
-    password text NOT NULL,
-    name character varying(30) NOT NULL,
-    "createDate" date
-);
-
-
-ALTER TABLE public.users OWNER TO jonas;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: jonas
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_id_seq OWNER TO jonas;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jonas
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: tokens id; Type: DEFAULT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.tokens ALTER COLUMN id SET DEFAULT nextval('public.tokens_id_seq'::regclass);
-
-
---
--- Name: urls id; Type: DEFAULT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- Data for Name: tokens; Type: TABLE DATA; Schema: public; Owner: jonas
---
-
-COPY public.tokens (id, user_id, token) FROM stdin;
-\.
-
-
---
--- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: jonas
---
-
-COPY public.urls (id, token, url, "shortUrl") FROM stdin;
-1	Bearer	https://chat.openai.com/chat	iKVLyWLd
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: jonas
---
-
-COPY public.users (id, email, password, name, "createDate") FROM stdin;
-1	rodrigo@test.com	$2b$10$hzHm4.mJ7rk1FGcjxsUvoeQosCnEVyASi/QyqeZmuRTQvYMf4dWay	rodrigo	2023-03-02
-\.
-
-
---
--- Name: tokens_id_seq; Type: SEQUENCE SET; Schema: public; Owner: jonas
---
-
-SELECT pg_catalog.setval('public.tokens_id_seq', 1, false);
-
-
---
--- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: jonas
---
-
-SELECT pg_catalog.setval('public.urls_id_seq', 1, true);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: jonas
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
-
-
---
--- Name: tokens tokens_pkey; Type: CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.tokens
-    ADD CONSTRAINT tokens_pkey PRIMARY KEY (id);
-
-
---
--- Name: tokens tokens_token_key; Type: CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.tokens
-    ADD CONSTRAINT tokens_token_key UNIQUE (token);
-
-
---
--- Name: tokens tokens_user_id_key; Type: CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.tokens
-    ADD CONSTRAINT tokens_user_id_key UNIQUE (user_id);
-
-
---
--- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.urls
-    ADD CONSTRAINT urls_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: tokens tokens_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: jonas
---
-
-ALTER TABLE ONLY public.tokens
-    ADD CONSTRAINT tokens_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: DEFAULT PRIVILEGES FOR SEQUENCES; Type: DEFAULT ACL; Schema: -; Owner: postgres
---
-
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres GRANT ALL ON SEQUENCES  TO jonas;
-
-
---
--- Name: DEFAULT PRIVILEGES FOR TYPES; Type: DEFAULT ACL; Schema: -; Owner: postgres
---
-
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres GRANT ALL ON TYPES  TO jonas;
-
-
---
--- Name: DEFAULT PRIVILEGES FOR FUNCTIONS; Type: DEFAULT ACL; Schema: -; Owner: postgres
---
-
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres GRANT ALL ON FUNCTIONS  TO jonas;
-
-
---
--- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: -; Owner: postgres
---
-
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres GRANT ALL ON TABLES  TO jonas;
-
-
---
--- PostgreSQL database dump complete
---
 
